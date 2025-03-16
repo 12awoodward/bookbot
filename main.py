054a24fa26efa42da5815e0afe22f871ffd8afae
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-03-16 16:49:18 +0000

    added get_book_text - read file contents

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+# returns content of file as str
+def get_book_text(file_path):
+  with open(file_path) as file:
+    return file.read()
+
+def main():
+  print(get_book_text("books/frankenstein.txt"))
+
+main()
