5d27e35a553871e6ed18b28fd085f0222b43cb0e
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-03-17 09:02:23 +0000

    refactor stats funcs

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import get_word_count
+
 # returns content of file as str
 def get_book_text(file_path):
   with open(file_path) as file:
     return file.read()
-
-# return number of words in str
-def get_word_count(content):
-  return len(content.split())
 
 def main():
   book_contents = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+# return number of words in str
+def get_word_count(content):
+  return len(content.split())
