1affd7b06b68cf411dbd0a207d33bc35ba52c33d
12awoodward <203260994+12awoodward@users.noreply.github.com>
2026-04-24 09:53:24 +0100

    formatting & typing

--- main.py
+++ main.py
@@ -1,55 +1,58 @@
-from stats import get_word_count
-from stats import get_char_counts
-from stats import sort_char_counts
 import sys
 
+from stats import get_word_count, get_char_counts, sort_char_counts
+
+
 # returns content of file as str
-def get_book_text(file_path):
-  with open(file_path) as file:
-    return file.read()
+def get_book_text(file_path: str):
+    with open(file_path) as file:
+        return file.read()
+
 
 # gen report str
-def gen_book_report(file_path):
-  end = "============= END ==============="
-  report_txt = "============ BOOKBOT ============\n"
-  report_txt += f"Analyzing book found at {file_path}...\n"
-  
-  try:
-    book_contents = get_book_text(file_path)
-  except FileNotFoundError:
-    report_txt += "\nCould not find book\n"
+def gen_book_report(file_path: str):
+    end = "============= END ==============="
+    report_txt = "============ BOOKBOT ============\n"
+    report_txt += f"Analyzing book found at {file_path}...\n"
+
+    try:
+        book_contents = get_book_text(file_path)
+    except FileNotFoundError:
+        report_txt += "\nCould not find book\n"
+        report_txt += end
+        return report_txt
+
+    if len(book_contents) == 0:
+        report_txt += "\nBook is empty\n"
+        report_txt += end
+        return report_txt
+
+    word_count = get_word_count(book_contents)
+    report_txt += "----------- Word Count ----------\n"
+    report_txt += f"Found {word_count} total words\n"
+
+    char_counts = sort_char_counts(get_char_counts(book_contents))
+    report_txt += "--------- Character Count -------\n"
+
+    for char_dict in char_counts:
+        if char_dict["char"].isalpha():
+            report_txt += f"{char_dict["char"]}: {char_dict["num"]}\n"
+
     report_txt += end
-    return report_txt
-  
-  if len(book_contents) == 0:
-    report_txt += "\nBook is empty\n"
-    report_txt += end
+
     return report_txt
 
-  word_count = get_word_count(book_contents)
-  report_txt += "----------- Word Count ----------\n"
-  report_txt += f"Found {word_count} total words\n"
-
-  char_counts = sort_char_counts(get_char_counts(book_contents))
-  report_txt += "--------- Character Count -------\n"
-  
-  for char_dict in char_counts:
-    if char_dict["char"].isalpha():
-      report_txt += f"{char_dict["char"]}: {char_dict["num"]}\n"
-
-  report_txt += end
-
-  return report_txt
 
 def main():
-  arg = sys.argv.pop()
-
-  if arg == "main.py":
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
-
-  while arg != "main.py":
-    print(gen_book_report(arg))
     arg = sys.argv.pop()
+
+    if arg == "main.py":
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    while arg != "main.py":
+        print(gen_book_report(arg))
+        arg = sys.argv.pop()
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,32 +1,40 @@
+from typing import TypedDict
+
+
 # return number of words in str
-def get_word_count(content):
-  return len(content.split())
+def get_word_count(content: str):
+    return len(content.split())
+
 
 # return dict of all char counts
-def get_char_counts(content):
-  counts = {}
+def get_char_counts(content: str):
+    counts: dict[str, int] = {}
 
-  for char in content:
-    char = char.lower()
-    if char in counts:
-      counts[char] += 1
-    else:
-      counts[char] = 1
+    for char in content:
+        char = char.lower()
+        if char in counts:
+            counts[char] += 1
+        else:
+            counts[char] = 1
 
-  return counts
+    return counts
+
+
+class CharCount(TypedDict):
+    char: str
+    num: int
+
+
+def sort_by_num(dict: CharCount):
+    return dict["num"]
+
 
 # return array of sorted dicts of chars and counts
-def sort_char_counts(counts_dict):
-  counts_array = []
+def sort_char_counts(counts_dict: dict[str, int]):
+    counts_array: list[CharCount] = []
 
-  for key in counts_dict:
-    counts_array.append({
-      "char": key,
-      "num": counts_dict[key]
-    })
+    for key in counts_dict:
+        counts_array.append({"char": key, "num": counts_dict[key]})
 
-  counts_array.sort(reverse=True, key=sort_by_num)
-  return counts_array
-
-def sort_by_num(dict):
-  return dict["num"]
+    counts_array.sort(reverse=True, key=sort_by_num)
+    return counts_array
